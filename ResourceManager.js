406e09e9ba540eb3527d8430b5b362a58e73582e
Cristian Lopez <cristian.lopez09825@gmail.com>
2022-03-23 20:44:35 -0500

    Validaciones agregadas al los edges

--- ResourceManager.js
+++ ResourceManager.js
@@ -1,59 +1,65 @@
-// nodos
 const nodes = new vis.DataSet([
-    {id: 1, label: 'Recurso 1', x: 150, y: 0}
-    , {id: 2, label: 'Recurso 2', x: 300, y: 0}
-    , {id: 3, label: 'Recurso 3', x: 450, y: 0}
-    , {id: 4, label: 'Proceso 1', x: 0, y: 200}
-    , {id: 5, label: 'Proceso 2', x: 150, y: 200}
-    , {id: 6, label: 'Proceso 3', x: 300, y: 200}
-    , {id: 7, label: 'Proceso 4', x: 450, y: 200}
-    , {id: 8, label: 'Proceso 5', x: 600, y: 200}
+    {id: "A", label: 'Recurso 1', x: 150, y: 0}
+    , {id: "B", label: 'Recurso 2', x: 300, y: 0}
+    , {id: "C", label: 'Recurso 3', x: 450, y: 0}
+    , {id: "1", label: 'Proceso 1', x: 0, y: 200}
+    , {id: "2", label: 'Proceso 2', x: 150, y: 200}
+    , {id: "3", label: 'Proceso 3', x: 300, y: 200}
+    , {id: "4", label: 'Proceso 4', x: 450, y: 200}
+    , {id: "5", label: 'Proceso 5', x: 600, y: 200}
 ]);
 
-function createDrawProcess() {
-    // edges
-    let edges = new vis.DataSet([]);
+let edges = new vis.DataSet([{from: "1", to: "A", arrows: "to"}]);
 
-    // acá creamos la _red_
+const options = {
+    interaction: {
+        selectable: false, dragNodes: false, dragView: false, zoomView: false,
+
+    },
+    nodes: {
+        font: {size: 15, color: "#000000"}, color: '#34495E', shape: 'square', size: 20
+    },
+    physics: {
+        enabled: false,
+    },
+};
+
+
+function createDrawProcess() {
     let container = document.getElementById('dibujo');
     let data = {
         nodes: nodes,
         edges: edges
-    };
-    let options = {
-        physics: {
-            enabled: false,
-        },
     };
     let network = new vis.Network(container, data, options);
 }
 
 function requestFunction(processToResource) {
+    /*edges.get().join(function (edge) {
+        if (edge.from === processToResource[0] && edge.to === processToResource[1]) {
+            console.log('NODO REGISTRADO PREVIAMENTE')
+        } else {
+            console.log('Nodo registrado')
+        }
+        //return edge.from === nodeId || edge.to === nodeId;
+    });*/
 
+    let exist = edges.get().filter(edge => edge.from === processToResource[0]
+        && edge.to === processToResource[1]);
 
-    // edges
-    let edges = new vis.DataSet([
-        {from: 1, to: 2, arrows: "to"}
-        , {from: 2, to: 3, arrows: "to"}
-        , {from: 10, to: 11, arrows: "to"}
-        , {from: 11, to: 12, arrows: "to"}
-        , {from: 21, to: 22, arrows: "to"}
-        , {from: 1, to: 10, arrows: "to", dashes: true}
-        , {from: 2, to: 11, arrows: "to", dashes: true}
-        , {from: 3, to: 12, arrows: "to", dashes: true}
-        , {from: 3, to: 21, arrows: "to"}
-    ]);
+    if (exist.length === 1) {
+        console.log('Ya agrego previamente esa ruta')
+    } else {
+        console.log(edges.get())
+        edges.add({from: processToResource[0], to: processToResource[1], arrows: "to"});
+    }
 
-    // acá creamos la _red_
+    edges.get().forEach(edge => console.log(edge));
+
     let container = document.getElementById('dibujo');
     let data = {
         nodes: nodes,
         edges: edges
-    };
-    let options = {
-        physics: {
-            enabled: false,
-        },
     };
     let network = new vis.Network(container, data, options);
 }
